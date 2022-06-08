6893e10a1568eb48bb25bb6da73efe2e3935f17a
Victor Almeida <44853244+umserqualquer@users.noreply.github.com>
2022-06-08 15:44:47 -0300

    update heroku

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,8 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
+
+
 Route::middleware(['auth'])->group(function() {
 
     Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
